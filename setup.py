05cb2c4cb2d7f00ea8e8b235ec608b8c57b5acf6
Paolo de Rosa <paolo.de.rosa@teamdigitale.governo.it>
2019-01-15 14:47:24 +0100

    create setup.py to install tofu as script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='tofu',
+      version='0.1',
+      description="""A TerraForm OpenStack dynamic inventory
+                   script that parses the JSON object from
+                   terraform state pull to generate a YAML or JSON
+                   blurb that is suitable for an ansible dynamic inventory""",
+      url='https://github.com/shalomb/tofu',
+      author='Shalom Bhooshi',
+      license='Apache License 2.0',
+      packages=['tofu'],
+      zip_safe=False,
+      scripts=['tofu/tofu.py']
+      )
