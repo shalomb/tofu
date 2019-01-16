590aa77dea78578d4e1dd42d507919eec4ae695f
Shalom Bhooshi <s.bhooshi@gmail.com>
2019-01-16 02:50:55 +0100

    Rename package to avoid conflict with existing pypi distribution

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ from setuptools import setup
 # pip install -e ./  # requires the following
 # pip install setuptools wheel
 
-setup(name='tofu',
+setup(name='tofu-openstack',
       version='0.1',
-      description="""A TerraForm OpenStack dynamic inventory
-                   script that parses the JSON object from
-                   terraform state pull to generate a YAML or JSON
-                   blurb that is suitable for an ansible dynamic inventory""",
+      description="A terraform openstack dynamic inventory for ansible",
       url='https://github.com/shalomb/tofu',
       author='Shalom Bhooshi',
       license='Apache License 2.0',
